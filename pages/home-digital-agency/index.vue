080b0112ac425314154745db0dc6d2c08d3aa144
samsul <samsul.marif100@gmail.com>
2023-07-06 21:50:10 +0800

    warna web

--- pages/home-digital-agency/index.vue
+++ pages/home-digital-agency/index.vue
@@ -18,21 +18,21 @@
 </template>
 
 <script>
-import TopNav from '../../components/Navbars/DigitalNav/TopNav';
-import Navbar from '../../components/Navbars/DigitalNav';
-import Header from '../../components/Digital/Header';
-import About from '../../components/Digital/About';
-import Services from '../../components/Digital/Services';
-import ChooseUs from '../../components/Digital/ChooseUs';
-import Portfolio from '../../components/Digital/Portfolio';
-import Testimonials from '../../components/Digital/Testimonials';
-import Team from '../../components/Digital/Team';
-import Blog from '../../components/Digital/Blog';
-import Contact from '../../components/Digital/Contact';
-import Footer from '../../components/Digital/Footer';
+import TopNav from "../../components/Navbars/DigitalNav/TopNav";
+import Navbar from "../../components/Navbars/DigitalNav";
+import Header from "../../components/Digital/Header";
+import About from "../../components/Digital/About";
+import Services from "../../components/Digital/Services";
+import ChooseUs from "../../components/Digital/ChooseUs";
+import Portfolio from "../../components/Digital/Portfolio";
+import Testimonials from "../../components/Digital/Testimonials";
+import Team from "../../components/Digital/Team";
+import Blog from "../../components/Digital/Blog";
+import Contact from "../../components/Digital/Contact";
+import Footer from "../../components/Digital/Footer";
 
 export default {
-  name: 'HomeDigitalAgency',
+  name: "HomeDigitalAgency",
   layout: "Main",
   components: {
     TopNav,
@@ -46,12 +46,12 @@ export default {
     Team,
     Blog,
     Contact,
-    Footer
+    Footer,
   },
   head() {
     return {
       titleTemplate: "%s - Digital Agency",
-    }
-  }
-}
+    };
+  },
+};
 </script>
